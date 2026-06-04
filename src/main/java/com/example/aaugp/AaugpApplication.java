c5161069395d743d71dd04a25e63b8816c527123
Kalkidan407 <kelemework267@gmail.com>
2026-06-04 13:16:34 -0400

    initial commit

---
+++ src/main/java/com/example/aaugp/AaugpApplication.java
@@ -0,0 +1,13 @@
+package com.example.aaugp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AaugpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AaugpApplication.class, args);
+	}
+
+}
