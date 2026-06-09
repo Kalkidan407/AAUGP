cf0ca8496c00444818811f57ac05338af1677c23
Kalkidan407 <kelemework267@gmail.com>
2026-06-09 08:19:20 -0400

    feat: add controller method get method

--- src/main/java/com/example/aaugp/controller/User.java
+++ src/main/java/com/example/aaugp/controller/User.java
@@ -1,6 +1,26 @@
 package com.example.aaugp.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
-public class User{}
+public class User{
+
+
+
+
+    @GetMapping("path")
+    public String getMethodName(@RequestParam String param) {
+        return new String();
+    }
+    
+    public String test() {
+        return "hello";
+    }
+
+
+
+}
