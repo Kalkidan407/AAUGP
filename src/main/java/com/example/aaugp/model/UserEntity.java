5c049dc446676aff7d41cf4e3c33f42155b1538d
Kalkidan407 <kelemework267@gmail.com>
2026-06-09 05:06:42 -0400

    fixing

--- src/main/java/com/example/aaugp/model/UserEntity.java
+++ src/main/java/com/example/aaugp/model/UserEntity.java
@@ -1,5 +1,7 @@
 package com.example.aaugp.model;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Column;
@@ -11,6 +13,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -35,10 +38,11 @@ public class UserEntity {
     @Column(nullable = false)
     private String password;    
 
-    @OneToOne(fetch = FetchType.LAZY)
-@JoinColumn(name = "project_id")
+
+
+@OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
 @JsonIgnore
-private ProjectEntity project;
+private List<ProjectEntity> projects;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
