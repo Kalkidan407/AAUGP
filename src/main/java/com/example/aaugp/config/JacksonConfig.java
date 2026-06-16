eef0a873656c9911edd9b52401890831ea5d0556
Kalkidan407 <kelemework267@gmail.com>
2026-06-15 21:57:50 -0400

    fix: try fixing object mapper issue

---
+++ src/main/java/com/example/aaugp/config/JacksonConfig.java
@@ -0,0 +1,15 @@
+package com.example.aaugp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+    @Configuration
+public class JacksonConfig {
+
+    @Bean
+    public com.fasterxml.jackson.databind.ObjectMapper objectMapper() {
+        return new com.fasterxml.jackson.databind.ObjectMapper();
+    }
+}
+
